d987d9752da26550ba79246e84f686e23ba9c5ab
Cristian Diaz <diazrcm@gmail.com>
2022-10-17 19:54:06 -0500

    este si

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -230,7 +230,8 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length < 1){
+  if (letra.length != 1){
+
     return "Dato incorrecto";
   } 
    if (letra != ('a' || 'e' || 'i' || 'o' || 'u' || 'A' || 'E' || 'I' || 'O' || 'U')){
@@ -240,7 +241,6 @@ function esVocal(letra){
     return "Es vocal";
   }
 }
-
 
 
 
