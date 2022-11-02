8323866b5332a650b0221cc09c965a0cc301ffe0
Cristian Diaz <diazrcm@gmail.com>
2022-11-01 21:45:35 -0500

    adelanto tarea

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -10,12 +10,14 @@ function devolverPrimerElemento(array) {
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return (array[array.length -1]);
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return (array.length);
 }
 
 
@@ -24,6 +26,9 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (let i=0; i<array.length; i++){
+    return (array + 1);
+  }
 }
 
 
@@ -31,6 +36,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
 }
 
 
@@ -39,6 +45,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
 }
 
 
