d8a606aa49d42635b4aec39b79a156cd435470a8
Cristian Diaz <diazrcm@gmail.com>
2022-11-08 20:19:49 -0500

    cuento elementos

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -130,7 +130,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let Y = [];
+  for (let i=0; i<arreglo.length ; i++){
+    if (arreglo[i]>18){
+      Y.push(arreglo[i]);
+    }
+  }
+  return Y.length;
 }
 
 
